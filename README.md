ea42d4e08d869adfd8f8d0f433381d23b8602ce5
RochaEverton <rochaevertondev@gmail.com>
2025-06-24 19:21:28 -0300

    Criado server e pagina do chat. Rodado testes localmente

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Chat_LabYes!
+- iniciado o projeto Node
+npm init -y
+
+- instalado as dependências:
+npm i express socket.io
+
+O Express.js é um framework Back-end Node que cria abstrações de rotas, middlewares e muitas outras funções para facilitar a criação tanto de API's quanto SPA's.
+
+O Socket.IO oferece uma API de JavaScript simples, baseada em eventos que te permite comunicar entre o servidor e o cliente sem esforço e em tempo real.
---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+  console.log('Usuário conectado');
+
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Usuário desconectado');
+  });
+});
+
+const PORT = process.env.PORT || 3030;
+http.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
